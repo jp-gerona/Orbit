16ce4ba6444ca0ec752aba60c8bf3860627c3a72
jp-gerona <julianpetergerona@gmail.com>
2024-02-11 02:05:33 +0800

    Retrieve userToken from login response

--- TwitterCloneUi/scripts/postUsers.js
+++ TwitterCloneUi/scripts/postUsers.js
@@ -13,9 +13,9 @@ export async function postCreateUser(formObject) {
       })
   })
   if(res.ok){
-      console.log("successful")
+      console.log('successful')
   } else {
-      console.log("registration fails")
+      console.log('registration fails')
   }
 }
 
@@ -23,7 +23,7 @@ export async function postLogIn() {
   var username = document.getElementById('username').value;
   var password = document.getElementById('password').value;
 
-  const res = await fetch("http://localhost:3000/api/v1/auth/login", {
+  const res = await fetch('http://localhost:3000/api/v1/auth/login', {
       method: 'POST',
       headers: {
           'Content-Type': 'application/json'
@@ -34,8 +34,10 @@ export async function postLogIn() {
       })
   })
   if(res.ok){
-      console.log("successful")
+      console.log('successful')
+      const userToken = await res.text();
+      console.log(`User token: ${userToken}`);
   } else {
-      console.log("login fails")
+      console.log('login fails')
   }
 }
