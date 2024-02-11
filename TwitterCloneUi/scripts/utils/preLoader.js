3ea15847a5d56468fd654026de739e09ae86acda
jp-gerona <julianpetergerona@gmail.com>
2024-02-11 17:16:56 +0800

    Fixed loader and added initial progress of home page

--- TwitterCloneUi/scripts/utils/preLoader.js
+++ TwitterCloneUi/scripts/utils/preLoader.js
@@ -1,8 +1,21 @@
 const loader = document.querySelector('.loader-section');
+const loaderIconWrapper = document.querySelector('.loader-icon-wrapper');
+const loaderLogo = document.querySelector('.loader-logo');
+
+function removeLoader() {
+  loader.remove(); 
+}
+
+function handleAnimationEnd() {
+  removeLoader();
+}
 
 window.addEventListener('load', () => {
   loader.classList.add('fade-out');
-  loader.addEventListener('animationend', () => {
-    loader.remove(); 
-  });
+  loaderIconWrapper.classList.add('zoom-out');
+  loaderLogo.classList.add('zoom-out');
+  
+  loader.addEventListener('animationend', handleAnimationEnd);
+  loaderIconWrapper.addEventListener('animationend', handleAnimationEnd);
+  loaderLogo.addEventListener('animationend', handleAnimationEnd);
 });
